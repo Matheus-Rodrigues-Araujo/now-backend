2ac4bdf0bb5607330ad9867d85e714ec58947db3
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-11-27 01:17:44 -0300

    feat: add ProjectMemberService to providers array

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { ProjectController } from "./project.controller";
 import { ProjectService } from "./project.service";
 import { PrismaService } from "src/prisma/prisma.service";
+import { ProjectMemberService } from "./project-member.service";
 
 @Module({
     controllers: [ProjectController],
-    providers: [ProjectService, PrismaService]
+    providers: [ProjectService, ProjectMemberService, PrismaService]
 })
 export class ProjectModule {}
