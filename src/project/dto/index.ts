0d577ff26776d773fb8ff9445b8dbf509093ac3e
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-11-27 01:19:43 -0300

    feat: add FindProjectDto

--- src/project/dto/index.ts
+++ src/project/dto/index.ts
@@ -1,2 +1,3 @@
 export { CreateProjectDto } from './create-project.dto';
-export { AddUsersToProjectDto } from "./add-users-to-project.dto"
+export { AddUsersToProjectDto } from './add-users-to-project.dto';
+export { FindProjectDto } from './find-project.dto';
