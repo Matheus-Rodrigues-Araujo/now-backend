8fe88e48d0a3a2a230a74cae6cdefc6dc05b93dc
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-11-30 00:48:56 -0300

    feat: new route handlers added

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Delete,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { CreateBoardDto } from './dto';
+import { BoardService } from './board.service';
+import { AuthenticateRequest } from 'src/types';
 
 @Controller('boards')
-export class BoardController {}
+@UseGuards(AuthGuard)
+export class BoardController {
+  constructor(private boardService: BoardService) {}
+
+  @Post()
+  async create(@Body() board: CreateBoardDto) {
+    return await this.boardService.create(board);
+  }
+
+  @Delete(':id')
+  async delete(
+    @Param('id') id: number,
+    @Body() body: any,
+    @Request() req: AuthenticateRequest,
+  ) {
+    const { projectId } = body;
+    await this.boardService.delete(id, projectId, req.user.sub);
+  }
+
+  //   @Get(':id')
+  //   async findAllBoardTasks(@Param('id', ParseIntPipe) id: number, @Body() body:any, @Request() req) {
+  //     return this.boardService.findAllBoardTasks(id, );
+  //   }
+}
