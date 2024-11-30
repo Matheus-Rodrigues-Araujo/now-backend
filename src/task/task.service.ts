833e08f30897536f03001fbb537092bdeb6340ab
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-11-30 02:44:59 -0300

    refactor(task.service): remove comments

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -8,33 +8,4 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class TaskService {
   constructor(private prismaService: PrismaService) {}
-
-  async findOne(id: number, projectId: number, userId: number) {
-    const userInProject = await this.prismaService.usersOnProjects.findFirst({
-      where: {
-        projectId: projectId,
-        userId: userId,
-      },
-      include: {
-        project: {
-          select: {
-            tasks: {
-              select: {
-                id: true,
-              },
-            },
-          },
-        },
-      },
-    });
-
-    if (!userInProject) throw new UnauthorizedException('Access denied');
-    if (userInProject.project.tasks.some((task) => task.id !== id))
-      throw new NotFoundException('Task not in the project');
-
-    const task = await this.prismaService.task.findUnique({ where: { id } });
-    if (!task) throw new NotFoundException('Task not found');
-
-    return task;
-  }
 }
