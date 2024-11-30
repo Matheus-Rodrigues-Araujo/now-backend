8eb7a863266c9db608394249549abfa43afb101c
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-11-30 02:44:06 -0300

    feat: add route handler to update the board

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -8,9 +8,10 @@ import {
   Delete,
   Request,
   UseGuards,
+  Put,
 } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
-import { CreateBoardDto } from './dto';
+import { CreateBoardDto, UpdateBoardDto } from './dto';
 import { BoardService } from './board.service';
 import { AuthenticateRequest } from 'src/types';
 
@@ -33,6 +34,17 @@ export class BoardController {
     const { projectId } = body;
     const { sub } = req.user;
     return await this.boardService.findAllBoardTasks(id, projectId, sub);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: { updateBoardDto: UpdateBoardDto; projectId: number },
+    @Request() req: AuthenticateRequest,
+  ) {
+    const { sub } = req.user;
+    const { updateBoardDto, projectId } = body;
+    return await this.boardService.update(updateBoardDto, id, projectId, sub);
   }
 
   @Delete(':id')
