6e46420e2a56eb18ed961297e31c198d618a168e
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-11-30 01:00:30 -0300

    feat(task.service): create findOne function

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(private prismaService: PrismaService) {}
+
+  async findOne(id: number, projectId: number, userId: number) {
+    const userInProject = await this.prismaService.usersOnProjects.findFirst({
+      where: {
+        projectId: projectId,
+        userId: userId,
+      },
+      include: {
+        project: {
+          select: {
+            tasks: {
+              select: {
+                id: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!userInProject) throw new UnauthorizedException('Access denied');
+    if (userInProject.project.tasks.some((task) => task.id !== id))
+      throw new NotFoundException('Task not in the project');
+
+    const task = await this.prismaService.task.findUnique({ where: { id } });
+    if (!task) throw new NotFoundException('Task not found');
+
+    return task;
+  }
+}
