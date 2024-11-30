f46984f4400dfcb628384c57fefb2e56c38ed194
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-11-30 02:43:38 -0300

    refactor: remove themObject and imported ThemeProps Interface

--- src/board/dto/create-board.dto.ts
+++ src/board/dto/create-board.dto.ts
@@ -1,10 +1,6 @@
 import { Transform } from 'class-transformer';
 import { IsString, IsNotEmpty, IsInt, IsJSON, IsObject } from 'class-validator';
-
-type themeObject = {
-  backgroundColor?: string;
-  color?: string;
-};
+import { ThemeProps } from 'src/types';
 
 export class CreateBoardDto {
   @IsString()
@@ -13,7 +9,7 @@ export class CreateBoardDto {
 
   @IsObject()
   @IsNotEmpty()
-  theme: themeObject;
+  theme: ThemeProps;
 
   @IsInt()
   @IsNotEmpty()
