606c347e5f806205731111007fce816b0f0923d7
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-01 02:01:01 -0300

    feat: create helper `formatProject`

---
+++ src/common/helpers/format-project.helper.ts
@@ -0,0 +1,18 @@
+import { ProjectModel, FormattedProject } from 'src/types';
+
+export function formatProject(
+  project: ProjectModel,
+  userId: number,
+): FormattedProject {
+  const formattedProject: FormattedProject = {};
+
+  const adminId = project.adminId;
+  delete project.adminId;
+
+  Object.entries(project).forEach(([key, value]) => {
+    formattedProject[key] = value;
+  });
+  formattedProject['isAdmin'] = adminId === userId;
+
+  return formattedProject;
+}
