befaf422aeda092395b9a88709455ec17975da67
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-01 01:59:22 -0300

    feat: export UpdateProjectDto

--- src/project/dto/index.ts
+++ src/project/dto/index.ts
@@ -1,3 +1,4 @@
 export { CreateProjectDto } from './create-project.dto';
 export { AddUsersToProjectDto } from './add-users-to-project.dto';
 export { FindProjectDto } from './find-project.dto';
+export { UpdateProjectDto } from "./update-project.dto"
