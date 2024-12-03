b051075b89d65e9283adf6dc25c3a60779482992
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-03 16:14:52 -0300

    refactor: rename interface AuthenticateRequest to JwtPayload

--- src/common/middlewares/project-authorization.middleware.ts
+++ src/common/middlewares/project-authorization.middleware.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { AuthenticateRequest } from 'src/types';
+import { JwtPayload } from 'src/types';
 import { validateUserOrAdmin } from '../validators';
 
 @Injectable()
@@ -9,7 +9,7 @@ export class ProjectAuthorizationMiddleware {
   constructor(private prismaService: PrismaService) {}
 
   async use(req: Request<any>, res: Response, next: NextFunction) {
-    const user = req.user as AuthenticateRequest['user'];
+    const user = req.user as JwtPayload['user'];
     const projectId = parseInt(req.params.projectId);
 
     if (!user) throw new UnauthorizedException('Invalid payload');
