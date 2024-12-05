9e17d8de29227374e0589fb33cb1b8095ee621d1
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-05 02:56:11 -0300

    feat(project): add service for admins

---
+++ src/project/services/admin-project.service.ts
@@ -0,0 +1,57 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { formatProject } from 'src/common/helpers';
+import { FormattedProject } from 'src/types';
+import { UpdateProjectDto } from '../dto';
+import { ProjectRepository } from '../project.repository';
+
+@Injectable()
+export class AdminProjectService {
+  constructor(private readonly projectRepository: ProjectRepository) {}
+
+  async addUsersToProject(
+    projectId: number,
+    adminId: number,
+    usersIds: number[],
+  ): Promise<{ message: string }> {
+    if (usersIds.length < 1)
+      throw new NotFoundException("You didn't add any user!");
+
+    if (usersIds.indexOf(adminId) !== -1)
+      throw new UnauthorizedException(
+        'Admin cannot be added as a regular user',
+      );
+
+    const addUsersToProject = await this.projectRepository.addUsersToProject(
+      projectId,
+      usersIds,
+    );
+    if (!addUsersToProject) throw new BadRequestException('Users not added');
+    return { message: 'User(s) added to project successfully!' };
+  }
+
+  async update(
+    updateProjectDto: UpdateProjectDto,
+    projectId: number,
+    userId: number,
+  ): Promise<FormattedProject> {
+    await this.projectRepository.findById(projectId);
+
+    const data = {};
+    Object.entries(updateProjectDto).forEach((obj) => {
+      data[obj[0]] = obj[1];
+    });
+
+    const project = await this.projectRepository.update(projectId, data);
+    return formatProject(project, userId);
+  }
+
+  async delete(projectId: number): Promise<{ message: string }> {
+    return await this.projectRepository.deleteWithDependencies(projectId);
+  }
+}
