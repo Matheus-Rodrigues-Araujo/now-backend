259aa986a033fd7907e0266f3704c599369ef143
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-05 02:53:19 -0300

    feat(project): add repository

---
+++ src/project/project.repository.ts
@@ -0,0 +1,117 @@
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
+import { Prisma, Project } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ProjectRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(data: Prisma.ProjectCreateInput): Promise<Project> {
+    return await this.prismaService.project.create({ data });
+  }
+
+  async findById(projectId: number): Promise<Project> {
+    const project = await this.prismaService.project.findUnique({
+      where: { id: projectId },
+    });
+
+    if (!project) throw new NotFoundException('Project not found');
+    return project;
+  }
+
+  async findByTitle(title: string): Promise<Project> {
+    const project = await this.prismaService.project.findFirst({
+      where: { title },
+    });
+
+    if (!project)
+      throw new NotFoundException(`Project with title ${title} not found`);
+    return project;
+  }
+
+  async findByTitleAndAdminId(title: string, adminId: number) {
+    return await this.prismaService.project.findFirst({
+      where: { title, adminId },
+    });
+  }
+
+  async findByIdOrTitle(query: {
+    id?: number;
+    title?: string;
+  }): Promise<Project> {
+    const { id, title } = query;
+
+    if (!id && !title)
+      throw new BadRequestException('ID or Title must be provided');
+
+    const project = this.prismaService.project.findFirst({
+      where: {
+        OR: [{ id: id || undefined }, { title: title || undefined }],
+      },
+    });
+    if (!project) throw new NotFoundException('Project not found');
+    return project;
+  }
+
+  async findMembers(projectId: number): Promise<any[]> {
+    return await this.prismaService.usersOnProjects.findMany({
+      where: { projectId },
+      include: {
+        user: {
+          select: {
+            id: true,
+            firstName: true,
+            lastName: true,
+            role: true,
+            image: true,
+            isActive: true,
+          },
+        },
+      },
+    });
+  }
+
+  async addUsersToProject(projectId: number, usersId: number[]) {
+    return await this.prismaService.usersOnProjects.createMany({
+      data: usersId.map((userId) => ({
+        userId,
+        projectId,
+      })),
+      skipDuplicates: true,
+    });
+  }
+
+  async update(
+    projectId: number,
+    data: Prisma.ProjectUpdateInput,
+  ): Promise<Project> {
+    const project = await this.prismaService.project.update({
+      where: { id: projectId },
+      data,
+    });
+    if (!project) throw new NotFoundException('Project not updated');
+
+    return project;
+  }
+
+  async deleteWithDependencies(
+    projectId: number,
+  ): Promise<{ message: string }> {
+    try {
+      await this.prismaService.$transaction([
+        this.prismaService.usersOnProjects.deleteMany({ where: { projectId } }),
+        this.prismaService.task.deleteMany({ where: { board: { projectId } } }),
+        this.prismaService.board.deleteMany({ where: { projectId } }),
+        this.prismaService.project.delete({ where: { id: projectId } }),
+      ]);
+
+      return { message: 'Project deleted succesfully' };
+    } catch {
+      throw new BadRequestException('The project could not be deleted');
+    }
+  }
+}
