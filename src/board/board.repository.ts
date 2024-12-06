01e8577668a7e3c8adabba8bfce988b2f55c44fc
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-06 02:26:54 -0300

    feat: add findBoardsFromProject

--- src/board/board.repository.ts
+++ src/board/board.repository.ts
@@ -1,12 +1,6 @@
-import {
-  BadRequestException,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Board, Prisma, Task } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { UpdateBoardDto } from './dto';
-import { validateUserOrAdmin } from 'src/common/validators';
 
 @Injectable()
 export class BoardRepository {
@@ -16,27 +10,23 @@ export class BoardRepository {
     return await this.prismaService.board.create({ data });
   }
 
-  async findOne(
-    boardId: number,
-    projectId: number,
-    userId: number,
-  ): Promise<Board> {
-    await validateUserOrAdmin(this.prismaService, projectId, userId);
-    const board = await this.prismaService.board.findUnique({
-      where: { id: boardId, projectId },
+  async findBoardsFromProject(projectId: number): Promise<Board[]> {
+    return await this.prismaService.board.findMany({
+      where: { projectId },
     });
-    if (!board) throw new NotFoundException('Board not found!');
-
-    return board;
   }
 
-  async findAllBoardTasks(
-    boardId: number,
-    projectId: number,
-    userId: number,
-  ): Promise<Task[]> {
-    await validateUserOrAdmin(this.prismaService, projectId, userId);
+  async findById(boardId: number, projectId: number): Promise<Board> {
+    return await this.prismaService.board.findUnique({
+      where: { id: boardId, projectId },
+    });
+  }
 
+  async findByTitle(title: string): Promise<Board> {
+    return await this.prismaService.board.findFirst({ where: { title } });
+  }
+
+  async findAllBoardTasks(boardId: number, projectId: number): Promise<Task[]> {
     const board = await this.prismaService.board.findFirst({
       where: { id: boardId, projectId },
       include: {
@@ -44,30 +34,18 @@ export class BoardRepository {
       },
     });
 
-    if (!board) throw new NotFoundException('Board not found');
-
     return board.tasks;
   }
 
   async update(
-    updateBoardDto: UpdateBoardDto,
     boardId: number,
     projectId: number,
-    userId: number,
+    data: Prisma.BoardUpdateInput,
   ): Promise<Board> {
-    const { title, theme } = updateBoardDto;
-    const dataToUpdate: Partial<Prisma.BoardUpdateInput> = {};
-
-    if (title !== undefined) dataToUpdate.title = title;
-    if (theme !== undefined) dataToUpdate.theme = theme as Prisma.JsonObject;
-
-    const updatedBoard = await this.prismaService.board.update({
+    return await this.prismaService.board.update({
       where: { id: boardId, projectId },
-      data: dataToUpdate,
+      data,
     });
-
-    if (!updatedBoard) throw new BadRequestException('Board not updated');
-    return await this.findOne(boardId, projectId, userId);
   }
 
   async updateOrder(boards: { id: number; order: number }[]): Promise<Board[]> {
@@ -81,21 +59,10 @@ export class BoardRepository {
     return await Promise.all(sortedBoards);
   }
 
-  async delete(
-    boardId: number,
-    projectId: number,
-    userId: number,
-  ): Promise<{ message: string }> {
-    // await validateAdmin(this.prismaService, projectId, userId);
-
-    try {
-      await this.prismaService.$transaction([
-        this.prismaService.task.deleteMany({ where: { boardId } }),
-        this.prismaService.board.delete({ where: { id: boardId } }),
-      ]);
-      return { message: 'Project deleted successfully' };
-    } catch (error) {
-      throw new BadRequestException(`Project could not be deleted: ${error}`);
-    }
+  async delete(boardId: number): Promise<void> {
+    await this.prismaService.$transaction([
+      this.prismaService.task.deleteMany({ where: { boardId } }),
+      this.prismaService.board.delete({ where: { id: boardId } }),
+    ]);
   }
 }
