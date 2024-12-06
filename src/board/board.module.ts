ad5c434c5beccc7e82c9fa85fcf75e972f5218be
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-06 00:47:08 -0300

    feat: add BoardAccessGuard to providers

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { BoardAccessGuard } from './guards/board-access.guard';
 
 @Module({
   controllers: [BoardController],
-  providers: [BoardService, PrismaService]
+  providers: [BoardService, PrismaService, BoardAccessGuard]
 })
 export class BoardModule {}
