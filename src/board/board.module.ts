8b0481da866f4ab9b0a814056634e1d9031b2788
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-06 02:28:44 -0300

    feat: add project guards and project repository in the providers

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { BoardAccessGuard } from './guards/board-access.guard';
+import { ProjectAdminGuard } from 'src/project/guards/project-admin.guard';
+import { ProjectGuard } from 'src/project/guards/project.guard';
+import { BoardRepository } from './board.repository';
+import { ProjectRepository } from 'src/project/project.repository';
 
 @Module({
   controllers: [BoardController],
-  providers: [BoardService, PrismaService, BoardAccessGuard]
+  providers: [
+    BoardService,
+    BoardRepository,
+    PrismaService,
+    ProjectGuard,
+    ProjectRepository,
+    ProjectAdminGuard,
+  ],
 })
 export class BoardModule {}
