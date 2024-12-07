ab3c7698b79bf565678744732e3220a38ffec2dc
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-07 19:28:31 -0300

    feat: create task.service

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -3,9 +3,34 @@ import {
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { TaskRepository } from './task.repository';
+import { Task } from '@prisma/client';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TaskService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(private readonly taskRepository: TaskRepository) {}
+
+  async findById(taskId: number, boardId: number): Promise<Task> {
+    const task = await this.taskRepository.findById(taskId, boardId);
+    if (!task) throw new NotFoundException('Task not found');
+    return task;
+  }
+
+  async createTask(boardId: number, createTaskDto: CreateTaskDto): Promise<Task> {
+    return await this.taskRepository.create(boardId, {
+      ...createTaskDto,
+      board: {
+        connect: {
+          id: boardId,
+        },
+      },
+    });
+  }
+
+  async deleteTask(boardId:number, taskId: number): Promise<void> {
+    const task = await this.taskRepository.findById(boardId, taskId)
+    if(!task) throw new NotFoundException("Task not found in this board")
+    await this.taskRepository.delete(taskId);
+  }
 }
