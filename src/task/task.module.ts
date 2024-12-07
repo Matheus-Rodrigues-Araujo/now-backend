846601d6fbacb48457058c8adb99185c5ca147b7
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-07 19:32:42 -0300

    feat: Inject projects dependencies

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { TaskRepository } from './task.repository';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ProjectAdminGuard } from 'src/project/guards/project-admin.guard';
+import { ProjectGuard } from 'src/project/guards/project.guard';
+import { ProjectRepository } from 'src/project/project.repository';
 
 @Module({
   controllers: [TaskController],
-  providers: [TaskService, PrismaService],
+  providers: [
+    TaskService,
+    TaskRepository,
+    PrismaService,
+    ProjectGuard,
+    ProjectAdminGuard,
+    ProjectRepository,
+  ],
 })
 export class TaskModule {}
