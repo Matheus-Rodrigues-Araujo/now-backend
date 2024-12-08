c9f46f21ed8833793ebb279bc7871341459c7c9b
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-08 14:26:44 -0300

    feat: add updateTask

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Put,
   UseGuards,
 } from '@nestjs/common';
 import { Task } from '@prisma/client';
@@ -13,6 +14,7 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskService } from './task.service';
 import { ProjectAdminGuard } from 'src/project/guards/project-admin.guard';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('projects/:projectId/boards/:boardId/tasks')
 @UseGuards(JwtAuthGuard)
@@ -28,6 +30,15 @@ export class TaskController {
     return await this.taskService.createTask(boardId, createTaskDto);
   }
 
+  @Put(':taskId')
+  @UseGuards(ProjectAdminGuard)
+  async updateTask(
+    @Param('taskId', ParseIntPipe) boardId: number,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ): Promise<Task> {
+    return await this.taskService.updateTask(boardId, updateTaskDto);
+  }
+
   @Delete(':taskId')
   @UseGuards(ProjectAdminGuard)
   async deleteTask(
@@ -37,5 +48,3 @@ export class TaskController {
     await this.taskService.deleteTask(boardId, taskId);
   }
 }
-
-//tasks/1/board/2
