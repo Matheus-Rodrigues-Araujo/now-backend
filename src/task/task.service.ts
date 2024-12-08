89972b8a010d9efa6607115126f22bc706d1acb0
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-08 14:26:09 -0300

    feat: add updateTask refactor: remove unused import and change findById to findTaskById

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,23 +1,23 @@
-import {
-  Injectable,
-  NotFoundException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { TaskRepository } from './task.repository';
 import { Task } from '@prisma/client';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TaskService {
   constructor(private readonly taskRepository: TaskRepository) {}
 
-  async findById(taskId: number, boardId: number): Promise<Task> {
+  async findTaskById(taskId: number, boardId: number): Promise<Task> {
     const task = await this.taskRepository.findById(taskId, boardId);
     if (!task) throw new NotFoundException('Task not found');
     return task;
   }
 
-  async createTask(boardId: number, createTaskDto: CreateTaskDto): Promise<Task> {
+  async createTask(
+    boardId: number,
+    createTaskDto: CreateTaskDto,
+  ): Promise<Task> {
     return await this.taskRepository.create(boardId, {
       ...createTaskDto,
       board: {
@@ -28,9 +28,16 @@ export class TaskService {
     });
   }
 
-  async deleteTask(boardId:number, taskId: number): Promise<void> {
-    const task = await this.taskRepository.findById(boardId, taskId)
-    if(!task) throw new NotFoundException("Task not found in this board")
+  async updateTask(
+    taskId: number,
+    updateTaskDto: UpdateTaskDto,
+  ): Promise<Task> {
+    return await this.taskRepository.update(taskId, updateTaskDto);
+  }
+
+  async deleteTask(boardId: number, taskId: number): Promise<void> {
+    const task = await this.taskRepository.findById(boardId, taskId);
+    if (!task) throw new NotFoundException('Task not found in this board');
     await this.taskRepository.delete(taskId);
   }
 }
