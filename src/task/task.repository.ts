b0dc3100cb3ab70c654a3c82599c47815f8d7abb
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-08 14:30:50 -0300

    refactor: change position of create and findById

--- src/task/task.repository.ts
+++ src/task/task.repository.ts
@@ -5,21 +5,18 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class TaskRepository {
   constructor(private prismaService: PrismaService) {}
-
+  
+  async create(boardId: number, data: Prisma.TaskCreateInput): Promise<Task> {
+    return await this.prismaService.task.create({ data });
+  }
+  
   async findById(boardId: number, taskId: number): Promise<Task> {
     return await this.prismaService.task.findFirst({
       where: { id: taskId, boardId },
     });
   }
 
-  async create(boardId: number, data: Prisma.TaskCreateInput): Promise<Task> {
-    return await this.prismaService.task.create({ data });
-  }
-
-  async update(
-    taskId: number,
-    data: Partial<Prisma.TaskUpdateInput>,
-  ): Promise<Task> {
+  async update(taskId: number, data: Prisma.TaskUpdateInput): Promise<Task> {
     return await this.prismaService.task.update({
       where: { id: taskId },
       data,
