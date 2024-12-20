ef5690e3a3ab5168350af73ca4983d03ed2b4c88
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-20 01:28:42 -0300

    feat: export Action_Type and Entity_Type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export { JwtPayload } from './payload';
 export { ProjectModel, FormattedProject } from './project';
+export { Action_Type, Entity_Type } from './history';
 export { ThemeProps } from './theme';
