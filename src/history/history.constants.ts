a1c7f35c8a9bdd6e8630e602f4a787e305349e8e
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-21 23:28:03 -0300

    feat: create history.constants

---
+++ src/history/history.constants.ts
@@ -0,0 +1,12 @@
+export enum Entity_Type {
+  USER = 'USER',
+  TASK = 'TASK',
+  PROJECT = 'PROJECT',
+  WORKSPACE = 'WORKSPACE',
+}
+
+export enum Action_Type {
+  CREATE = 'CREATE',
+  UPDATE = 'UPDATE',
+  DELETE = 'DELETE',
+}
