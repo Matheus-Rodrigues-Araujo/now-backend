baa48055dcb87413fad9a77370707e1a0ca6f86e
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-21 23:27:46 -0300

    refactor: removed exports from './history'

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,3 @@
 export { JwtPayload } from './payload';
 export { ProjectModel, FormattedProject } from './project';
-export { Action_Type, Entity_Type } from './history';
 export { ThemeProps } from './theme';
