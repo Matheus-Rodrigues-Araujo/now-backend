a871b5d4fa72933773979b53f06a0ff886538c85
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-23 14:49:20 -0300

    feat: export interfaces from index.ts

---
+++ src/common/interfaces/index.ts
@@ -0,0 +1,4 @@
+export { JwtPayload } from './payload.interface';
+export { ProjectModel, FormattedProject } from './project.interface';
+export { ThemeProps } from './theme.interface';
+export { IRepository } from './repository.interface';
