bca698f05f21ea033b0a68816e521d9274f38f23
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-28 01:53:39 -0300

    feat: add new documentation with Swagger

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,13 +3,22 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { JwtPayload } from 'src/common/interfaces';
 import { UserService } from './user.service';
 import { CurrentUser } from 'src/common/decorators';
+import {
+  ApiBadRequestResponse,
+  ApiOkResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @Controller('users')
 @UseGuards(JwtAuthGuard)
 export class UserController {
   constructor(private userService: UserService) {}
 
+  /**
+   * Get user information
+   */
   @Get('me')
+  @ApiUnauthorizedResponse()
   me(@CurrentUser() user: JwtPayload['user']) {
     const { firstName, lastName } = user;
     return {
@@ -18,7 +27,13 @@ export class UserController {
     };
   }
 
+  /**
+   * Find all the user's projects
+   */
   @Get('projects')
+  @ApiOkResponse({ type: Object, isArray: true })
+  @ApiBadRequestResponse({ description: 'Faild to fetch project members' })
+  @ApiUnauthorizedResponse({ description: 'Access denied' })
   async findProjectsForUser(@CurrentUser() user: JwtPayload['user']) {
     const userId = user.sub;
     return this.userService.findProjectsForUser(userId);
