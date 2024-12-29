d62c1d2bd186b4e4a4066e89f1a6d6fe968d7157
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-28 23:56:58 -0300

    feat: add new custom decorators to handle API responses/requests

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,45 +1,26 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthLoginDto, AuthRegisterDto } from './dto';
 import { AuthService } from './auth.service';
 import { User } from '@prisma/client';
-import {
-  ApiBadRequestResponse,
-  ApiOkResponse,
-  ApiUnauthorizedResponse,
-} from '@nestjs/swagger';
-
+import { LoginSwagger, RegisterSwagger } from './swagger';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  /**
-   * Log in the user
-   */
   @Post('login')
-  @ApiOkResponse({ type: Object, description: 'Login successful' })
-  @ApiBadRequestResponse({ description: 'Could not sign in' })
-  @ApiUnauthorizedResponse({ description: 'Invalid email or password' })
+  @LoginSwagger.operation
+  @LoginSwagger.okResponse
+  @LoginSwagger.badRequest
+  @LoginSwagger.unauthorized
   async login(@Body() authLogin: AuthLoginDto) {
     return await this.authService.login(authLogin);
   }
 
-  /**
-   * Register new user
-   */
   @Post('register')
-  @ApiOkResponse({ type: Object, description: 'Login successful' })
-  @ApiBadRequestResponse({ description: 'User not registered' })
-  @ApiUnauthorizedResponse({
-    description:
-      'A user account with the provided email or username already exists.',
-  })
+  @RegisterSwagger.operation
+  @RegisterSwagger.okResponse
+  @RegisterSwagger.badRequest
+  @RegisterSwagger.unauthorized
   async register(@Body() authRegister: AuthRegisterDto): Promise<User> {
     return await this.authService.register(authRegister);
   }
