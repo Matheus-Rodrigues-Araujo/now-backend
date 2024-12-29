82fa55e067c4592dcb229ac2901e982bff00d22a
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-12-28 23:57:13 -0300

    chore: delete unnecessary file

--- src/common/decorators/current-user-decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { JwtPayload } from 'src/common/interfaces';
-
-export const CurrentUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    const request = ctx.switchToHttp().getRequest();
-    return request.user as JwtPayload['user'];
-  },
-);
