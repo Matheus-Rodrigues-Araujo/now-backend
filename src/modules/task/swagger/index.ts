a4c517fe5ffa42592fb7303c2c447f4e4f7a040e
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2025-01-16 16:08:21 -0300

    feat: export `.swagger.ts` files

---
+++ src/modules/task/swagger/index.ts
@@ -0,0 +1,4 @@
+export { CreateTaskSwagger } from './create.swagger';
+export { UpdateTaskSwagger } from './update.swagger';
+export { DeleteTaskSwagger } from './delete.swagger';
+export { MoveTaskToBoardSwagger } from './move-task-to-board.swagger';
