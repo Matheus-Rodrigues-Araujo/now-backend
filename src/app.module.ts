2196ba9097fb4c9566bff25250eacfd41beeff23
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2025-01-16 15:38:16 -0300

    refactor: update new files path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user/user.module';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { BoardModule } from './board/board.module';
-import { TaskModule } from './task/task.module';
-import { ProjectModule } from './project/project.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { AuthModule } from './modules//auth/auth.module';
+import { BoardModule } from './modules//board/board.module';
+import { TaskModule } from './modules//task/task.module';
+import { ProjectModule } from './modules//project/project.module';
 
 @Module({
   imports: [
